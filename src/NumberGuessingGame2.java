c5dac8c723d1d5b1213c57568da78bbdf3d73369
Jayasri <Jayasri.Sundararajan.2022@student.lhschools.org>
2018-09-27 13:15:42 -0400

    Started Number Guessing Game 2

---
+++ src/NumberGuessingGame2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class NumberGuessingGame2 {
+
+    public static void main(String[]args) {
+
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+    }
+}
