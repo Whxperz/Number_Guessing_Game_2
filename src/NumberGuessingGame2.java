86eba657e01a3fd96e62604c8d227f672802edb1
Jayasri <Jayasri.Sundararajan.2022@student.lhschools.org>
2018-09-27 14:15:46 -0400

    Auto stash before merge of "master" and "origin/master"

--- src/NumberGuessingGame2.java
+++ src/NumberGuessingGame2.java
@@ -7,5 +7,38 @@ public class NumberGuessingGame2 {
         Scanner keyboard;
         keyboard = new Scanner(System.in);
 
+<<<<<<< Updated upstream
     }
 }
+=======
+        //Declare
+
+        int pickedNumber;
+        String input;
+        pickedNumber = (int)(Math.random()*100);
+
+        //Code
+        System.out.println("Pick a number");
+        int guess;
+        do {
+            guess = keyboard.nextInt();
+            if (guess > pickedNumber) {
+                System.out.println("Too high, guess again");
+            } else if (guess < pickedNumber) {
+                System.out.println("Too low, guess again");
+            } else if (guess == pickedNumber) {
+                System.out.println("Good job! The number is " + guess + "!");
+            }
+        }  while (guess != pickedNumber);
+
+        System.out.println("Would you like to play again?");
+        while keyboard.nextLine() = "Yes";
+
+
+
+    }
+}
+
+
+
+>>>>>>> Stashed changes
